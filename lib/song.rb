864c1c4c0596b3c778636987c2df82f20099d292
brianwlego <legobw@gmail.com>
2020-07-16 20:28:03 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,61 @@ class Song
   def self.all
     @@all
   end
-
   def save
     self.class.all << self
   end
+
+  def self.create
+     song = self.new
+     song.save
+     song 
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name=name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) 
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(file)
+    split_array = file.delete_suffix(".mp3").split(" - ")
+    song = self.new
+    song.name = split_array[1]
+    song.artist_name = split_array[0]
+    song
+  end
+
+  def self.create_from_filename(file)
+    song = self.new_from_filename(file)
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 
 end
